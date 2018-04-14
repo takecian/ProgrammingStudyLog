22ccdfa8afdb68626c5b8b4c0d61837401713a0a
takecian <takecian@gmail.com>
2018-04-14 23:49:02 +0900

    Add snippet

---
+++ snippet.py
@@ -0,0 +1,22 @@
+# create sample data
+import random
+n = 10**1
+
+print(n)
+
+d = []
+for i in range(n):
+    d.append(str(random.randint(0, 10**9)))
+
+print(" ".join(d))
+
+
+# input
+
+count = int(input())    # number of input
+for i in range(count):  # loop for each input
+    data = input()
+    # do something
+
+#
+
