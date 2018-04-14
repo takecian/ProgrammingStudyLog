806ff46607fab85b3c2e0619c81eaec02164be71
takecian <takecian@gmail.com>
2018-04-14 16:51:08 +0900

    Solve

---
+++ projecteuler/11-20/problem13.py
@@ -0,0 +1,11 @@
+# https://projecteuler.net/problem=13
+
+data = open("input12.txt", "r")
+
+lines = list(map(int, data.readlines()))
+
+data.close()
+
+print(sum(lines))
+
+# 5537376230390876637302048746832985971773659831892672
