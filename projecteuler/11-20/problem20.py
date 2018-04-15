bf2c57fff17f56baa436f19066d0772cce8fe71e
takecian <takecian@gmail.com>
2018-04-15 09:59:45 +0900

    Solve

---
+++ projecteuler/11-20/problem20.py
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=20
+#
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+# Find the sum of the digits in the number 100!
+
+
+def permutuation(n):
+    if n == 1: return 1
+    return n * permutuation(n - 1)
+
+
+print(sum(map(int, list(str(permutuation(100))))))
