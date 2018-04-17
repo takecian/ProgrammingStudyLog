200a6f86d798da39de27c78d86ddb5f7fe8e6a2b
takecian <takecian@gmail.com>
2018-04-17 16:03:04 +0900

    Tweak

--- projecteuler/31-40/31.py
+++ projecteuler/31-40/31.py
@@ -4,15 +4,13 @@ coins = [200, 100, 50, 20, 10, 5, 2, 1]
 
 
 def made_up(cl, v):
-    if len(cl) == 0:
-        return 0
     total = 0
     c = cl.pop(0)
     i = 0
     while c * i <= v:
         if c * i < v:
-            tv = v - c * i
-            total += made_up(list(cl), tv)
+            if len(cl) > 0:
+                total += made_up(list(cl), v - c * i)
         if c * i == v:
             total += 1
             break
