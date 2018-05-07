cca982a23ede6cfd288e92f2a12f63affcbb10eb
takecian <takecian@gmail.com>
2018-05-07 23:23:51 +0900

    Solve

--- projecteuler/51-60/55.py
+++ projecteuler/51-60/55.py
@@ -1,2 +1,27 @@
 # https://projecteuler.net/problem=55
 
+
+def calc(n):
+    r = int(str(n)[::-1])
+    return n + r
+
+
+def is_palindromic(n):
+    return str(n) == str(n)[::-1]
+
+
+count = 0
+for i in range(1, 10001):
+    is_p = False
+
+    v = i
+    for j in range(50):
+        v = calc(v)
+        if is_palindromic(v):
+            print(str(i) + ", pand = " + str(v))
+            is_p = True
+            break
+    if is_p:
+        count += 1
+
+print(10000 - count)
