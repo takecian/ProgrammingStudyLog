c7636768ce6b65aaee6fa6164ea9bf2308b6cf0b
takecian <takecian@gmail.com>
2018-07-04 23:13:56 +0900

    wip

--- AtCoder/Recommended/085c.py
+++ AtCoder/Recommended/085c.py
@@ -2,6 +2,18 @@
 
 N, Y = map(int, input().split())
 
-x = -1
-y = -1
-z = -1
+
+def solve(n, y):
+    for a in range(N + 1):
+        rest = y - 10000 * a
+        if rest == 0:
+            return (a, 0, 0)
+        bc = n - a
+        cand_b = rest // 1000
+
+
+    return (-1, -1, -1)
+
+
+result = solve(N, Y)
+print(str(result[0]) + ' ' + str(result[1]) + ' ' + str(result[2]))
