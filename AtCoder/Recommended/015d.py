d2a6e5eea57647d33ade558782968652bf712be6
takecian <takecian@gmail.com>
2018-07-13 16:00:01 +0900

    Wip

--- AtCoder/Recommended/015d.py
+++ AtCoder/Recommended/015d.py
@@ -21,9 +21,10 @@ dp = [[[0 for _ in range(W + 1)] for _ in range(K + 1)] for _ in range(N + 1)]
 for i in range(1, N + 1):
     for k in range(K + 1):
         for w in range(W + 1):
-            if A[i] <= w and i <= k:
-                dp[i][k][w] = max(dp[i - 1][k][w], dp[i - 1][k - 1][w - A[i]] + B[i])
+            if A[i] <= w:
+                dp[i][k][w] = max(dp[i - 1][k - 1][w], dp[i - 1][k - 1][w - A[i]] + B[i])
             else:
-                dp[i][k][w] = dp[i - 1][k][w]
+                dp[i][k][w] = dp[i - 1][k - 1][w]
 
+# print(dp)
 print(dp[N][K][W])
