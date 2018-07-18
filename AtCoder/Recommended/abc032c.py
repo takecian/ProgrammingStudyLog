fa973ba60c8d537891b300ccabc9f7bb2be6b641
takecian <takecian@gmail.com>
2018-07-18 16:49:32 +0900

    Solve

--- AtCoder/Recommended/abc032c.py
+++ AtCoder/Recommended/abc032c.py
@@ -9,20 +9,27 @@ if 0 in S:
     print(len(S))
     exit(0)
 
+if min(S) > K:
+    print(0)
+    exit(0)
+
+left = 0
+right = 0
 answer = 0
+res = 1
 
-for left in range(N):
-    temp = 1
-    right = left
-    while right < N:
-        temp *= S[right]
-        if temp > K:
-            if right - left > answer:
-                answer = right - left
-            break
+while left < N and right < N:
+    if res <= K:
+        answer = max(answer, right - left)
+        res *= S[right]
         right += 1
+    else:
+        res /= S[left]
+        left += 1
+        if res <= K:
+            answer = max(answer, right - left)
 
-    if right - left > answer:
-        answer = right - left
+if res <= K:
+    answer = max(answer, right - left)
 
 print(answer)
