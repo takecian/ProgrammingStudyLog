cb8d139a3c95e4e434a421de3b0aaf0b957f2347
takecian <takecian@gmail.com>
2019-01-25 13:51:58 +0900

    Make euler answers private

--- projecteulerback/11-20/problem16.py
+++
@@ -1,3 +0,0 @@
-# https://projecteuler.net/problem=16
-
-print(sum(map(int, list(str(2**1000)))))
--- projecteulerback/41-50/48.py
+++
@@ -1,7 +0,0 @@
-# https://projecteuler.net/problem=48
-
-total = 0
-for i in range(1,1001):
-    total += i ** i
-
-print(str(total)[-10::])
