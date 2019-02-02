1f2d06bdd33b47433f76533b071060b6f686b6a4
takecian <takecian@gmail.com>
2019-02-02 23:05:46 +0900

    Solve

--- LeetCode/14.py
+++ LeetCode/14.py
@@ -1,2 +1,31 @@
 # https://leetcode.com/problems/longest-common-prefix/
 
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        answer = ""
+        if len(strs) == 0:
+            return answer
+
+        min_length = min(map(lambda s: len(s), strs))
+        for i in range(min_length):
+            c = strs[0][i]
+            can_add = True
+            for s in strs:
+                if s[i] != c:
+                    can_add = False
+            if can_add:
+                answer += c
+            else:
+                break
+
+        return answer
+
+
+s = Solution()
+print(s.longestCommonPrefix(["aca","cba"]))
+# print(s.longestCommonPrefix(["flower","flow","flight"]))
+# print(s.longestCommonPrefix( ["dog","racecar","car"]))
