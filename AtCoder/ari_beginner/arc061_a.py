73d78bf856010c7dd42bcc47cabaebd1161b8d66
takecian <takecian@gmail.com>
2019-02-04 16:58:16 +0900

    wip

---
+++ AtCoder/ari_beginner/arc061_a.py
@@ -0,0 +1,14 @@
+# https://arc061.contest.atcoder.jp/tasks/arc061_a
+
+S = list(input())
+print(S)
+
+answer = 0
+for i in range(1 << (len(S)-1)):
+    expression = []
+    for j in range(len(S)):
+        if ((i >> j) & 1) == 1:
+            expression.append("{}+".format(S[j]))
+        else:
+            expression.append(S[j])
+    print("".join(expression))
