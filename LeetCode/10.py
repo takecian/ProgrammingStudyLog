76086444872db04285110c9323d72bfaf577fe65
takecian <takecian@gmail.com>
2019-02-05 15:46:14 +0900

    Solve problem

---
+++ LeetCode/10.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
+
+class Solution:
+    def isMatch(self, s: 'str', p: 'str') -> 'bool':
+        if len(p) == 0:
+            return len(s) == 0
+
+        is_first_matched = len(s) > 0 and p[0] in { s[0], "." }
+        if len(p) >= 2 and p[1] == "*":
+            return (self.isMatch(s, p[2:])) or (is_first_matched and self.isMatch(s[1:], p))
+        else:
+            return is_first_matched and self.isMatch(s[1:], p[1:])
+
+
+s = Solution()
+print(s.isMatch("aabc", "c*a*b"))
+
