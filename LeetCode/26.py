cda29c5df34af23ac8d3afd73195496afc4fd9c0
takecian <takecian@gmail.com>
2019-02-05 19:00:49 +0900

    Solve problem

---
+++ LeetCode/26.py
@@ -0,0 +1,16 @@
+import sys
+
+class Solution:
+    def removeDuplicates(self, nums: 'List[int]') -> 'int':
+        i = 0
+        p = -123
+        while i < len(nums):
+            if p != nums[i]:
+                p = nums[i]
+                i += 1
+            else:
+                del nums[i]
+        return len(nums)
+
+s = Solution()
+print(s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
