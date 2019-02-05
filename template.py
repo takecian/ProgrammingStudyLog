c94864f31201487e85128935dc620f1af5977da8
takecian <takecian@gmail.com>
2019-02-05 23:00:53 +0900

    Add template

---
+++ template.py
@@ -0,0 +1,15 @@
+
+dx4 = [0, 0, 1, -1]
+dy4 = [1, -1, 0, 0]
+
+for dx, dy in zip(dx4, dy4):
+    break
+
+
+dx8 = [0, 0, 1, -1, 1, -1, 1, -1]
+dy8 = [1, -1, 0, 0, 1, 1, -1, -1]
+
+for dx, dy in zip(dx8, dy8):
+    break
+
+
