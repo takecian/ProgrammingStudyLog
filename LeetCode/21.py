a7fd047df536ef957b903731a43e0cb63202dd91
takecian <takecian@gmail.com>
2019-02-05 17:28:01 +0900

    Solve problem

--- LeetCode/21.py
+++ LeetCode/21.py
@@ -2,43 +2,28 @@
 
 class Solution:
     def mergeTwoLists(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
-        head = None
+        head = ListNode(0)
         head1 = l1
         head2 = l2
-        if not head1 and head2:
-            head = ListNode(head2.val)
-            head2 = head2.next
-        elif head1 and not head2:
-            head = ListNode(head1.val)
-            head1 = head1.next
 
-        if head1 and head2:
-            if head1.val < head2.val:
-                head = ListNode(head1.val)
-                head1 = head1.next
-            else:
-                head = ListNode(head2.val)
-                head2 = head2.next
         top = head
-        while head1 or head2:
-            if not head1:
-                head.next = ListNode(head2.val)
-                head = head.next
-                head2 = head2.next
-            elif not head2:
+        while head1 and head2:
+            if head1.val < head2.val:
                 head.next = ListNode(head1.val)
                 head = head.next
                 head1 = head1.next
             else:
-                if head1.val < head2.val:
-                    head.next = ListNode(head1.val)
-                    head = head.next
-                    head1 = head1.next
-                else:
-                    head.next = ListNode(head2.val)
-                    head = head.next
-                    head2 = head2.next
-        return top
+                head.next = ListNode(head2.val)
+                head = head.next
+                head2 = head2.next
+
+        if head1:
+            head.next = head1
+
+        if head2:
+            head.next = head2
+
+        return top.next
 
 s = Solution()
 s.mergeTwoLists()
