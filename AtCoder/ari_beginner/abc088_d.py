9aad2498fc7288772675931d07ea1cc8330e2944
takecian <takecian@gmail.com>
2019-02-06 14:34:58 +0900

    Solve problem

--- AtCoder/ari_beginner/abc088_d.py
+++ AtCoder/ari_beginner/abc088_d.py
@@ -2,13 +2,13 @@
 
 H, W = map(int, input().split())
 s = [input() for _ in range(H)]
-d = [[-1 for _ in range(W)] for _ in range(W)]
+d = [[-1] * W for _ in range(H)]
 
 # 0,0 to h-1, w-1
 d[0][0] = 0
 
 queue = []
-queue.append((0, 0))
+queue.append([0, 0])
 
 while len(queue) > 0:
     p = queue.pop(0)
@@ -22,13 +22,18 @@ while len(queue) > 0:
         next_x = x + dx
         if 0 <= next_y < H and 0 <= next_x < W and s[next_y][next_x] != "#" and d[next_y][next_x] == -1:
             d[next_y][next_x] = d[y][x] + 1
-            queue.append((next_y, next_x))
+            queue.append([next_y, next_x])
 
 count = 0
 for l in s:
-    count += l.count(".")
+    for c in l:
+        if c == ".":
+            count += 1
 
-print(count - d[H-1][W-1] - 1)
+if d[H-1][W-1] == -1:
+    print(-1)
+else:
+    print(count - d[H-1][W-1] - 1)
 
 # [print(l) for l in s]
 # [print(l) for l in d]
