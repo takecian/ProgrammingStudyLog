5d0a9f22834f774f520845e6570d13637b3d509b
takecian <takecian@gmail.com>
2019-02-06 23:56:47 +0900

    Update

--- AtCoder/ari_beginner/arc005_3.py
+++ AtCoder/ari_beginner/arc005_3.py
@@ -16,10 +16,9 @@ g = find("g")
 
 
 def bfs(s, g):
-    d = [[[-1 for _ in range(3)] for _ in range(W)] * W for _ in range(H)]
+    d = [[3] * W for _ in range(H)]
 
-    d[s[0]][s[1]][0] = 0
-
+    d[s[0]][s[1]] = 0
 
     que = []
     que.append((s[0], s[1], 0))
@@ -34,13 +33,13 @@ def bfs(s, g):
         for dx, dy in zip(dx4, dy4):
             next_y = current[0] + dy
             next_x = current[1] + dx
-            if 0 <= next_y < H and 0 <= next_x < W and d[next_y][next_x][current[2]] == -1:
-                if C[next_y][next_x] == "#" and current[2] < 2:
-                    que.append((next_y, next_x, current[2] + 1))
-                    d[next_y][next_x][current[2] + 1] = d[current[0]][current[1]][current[2]] + 1
-                elif C[next_y][next_x] == ".":
-                    que.append((next_y, next_x, current[2]))
-                    d[next_y][next_x][current[2]] = d[current[0]][current[1]][current[2]] + 1
+            if 0 <= next_y < H and 0 <= next_x < W:
+                if C[next_y][next_x] == "#" and d[current[0]][current[1]] < 2 and d[current[0]][current[1]] + 1 < d[next_y][next_x]:
+                    que.append((next_y, next_x))
+                    d[next_y][next_x] = d[current[0]][current[1]] + 1
+                elif C[next_y][next_x] == "." and d[current[0]][current[1]] < d[next_y][next_x]:
+                    que.append((next_y, next_x))
+                    d[next_y][next_x] = d[current[0]][current[1]]
                 elif C[next_y][next_x] == "g":
                     return "YES"
 
