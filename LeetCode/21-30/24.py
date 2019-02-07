c76fa6a290323014cf83fc169066b7b8193b6146
takecian <takecian@gmail.com>
2019-02-07 19:36:27 +0900

    Solve problem

---
+++ LeetCode/21-30/24.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def swapPairs(self, head: 'ListNode') -> 'ListNode':
+        if not head or not head.next:
+            return head
+
+        dummy = ListNode(0)
+
+        zero = dummy
+        first = None
+
+        cursor = head
+        while cursor:
+            if not first:
+                first = cursor
+                cursor = cursor.next
+            else:
+                second = cursor
+                zero.next = second
+                cursor = cursor.next
+                second.next = first
+                first.next = cursor
+                zero = first
+                first = None
+
+        return dummy.next
---
+++ LeetCode/21-30/28.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/implement-strstr/
+
+class Solution:
+    def strStr(self, haystack: 'str', needle: 'str') -> 'int':
+        return haystack.find(needle)
