9b455bbf477237b6743039f986e52d6139275f5f
takecian <takecian@gmail.com>
2019-02-08 17:49:48 +0900

    wip

--- LeetCode/31-40/32.py
+++ LeetCode/31-40/32.py
@@ -3,4 +3,22 @@
 
 class Solution:
     def longestValidParentheses(self, s):
+        longest = 0
+        current = 0
+        left_counter = 0
+        for c in s:
+            if c == "(":
+                left_counter += 1
+            else: # ")"
+                if left_counter > 0:
+                    left_counter -= 1
+                    current += 1
+                    if left_counter == 0:
+                        longest = max(current, longest)
+                else:
+                    current = 0
 
+        return longest * 2
+
+s = Solution()
+print(s.longestValidParentheses("(()"))
