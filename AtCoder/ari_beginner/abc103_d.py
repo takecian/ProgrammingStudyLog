ea36900c8190246197b2e7d61b4ed47d78924f79
takecian <takecian@gmail.com>
2019-02-08 18:25:42 +0900

    Solve problem

--- AtCoder/ari_beginner/abc103_d.py
+++ AtCoder/ari_beginner/abc103_d.py
@@ -4,4 +4,17 @@
 N, M = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(M)]
 
+a.sort(key=lambda x: x[1])
 
+# print(a)
+
+count = 0
+
+g = 0
+for p in a:
+    if g <= p[0]:
+        count += 1
+        g = p[1]
+        # print(p)
+
+print(count)
