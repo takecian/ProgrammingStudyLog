3e81e3807e55dac28369b095654d78df30177b2b
takecian <takecian@gmail.com>
2019-02-21 14:53:52 +0900

    Solve problem

--- AtCoder/abc109/c.py
+++ AtCoder/abc109/c.py
@@ -1,12 +1,22 @@
 # https://atcoder.jp/contests/abc109/tasks/abc109_c
 
+def gcd(a, b):
+  if b == 0:
+    return a
+  else:
+    return gcd(b, a % b)
+
+
 def main():
     N, X = map(int, input().split())
 
     xl = list(map(int, input().split()))
 
-    diff_x = a
+    ans = abs(X - xl[0])
+    for i in range(1, len(xl)):
+        ans = gcd(max(ans, abs(X - xl[i])), min(ans, abs(X - xl[i])))
+
+    print(ans)
+
 
 main()
-
-# X - x
