68374dcb6fd3c805072bde01eeec714585f8a4d5
takecian <takecian@gmail.com>
2019-02-26 19:45:34 +0900

    wip

---
+++ AtCoder/ABC/070/c.py
@@ -0,0 +1,14 @@
+# https://abc070.contest.atcoder.jp/tasks/abc070_c
+
+import itertools
+import collections
+from bisect import bisect
+
+def main():
+    N = int(input())
+    T = [int(input()) for _ in range(N)]
+
+
+
+if __name__ == '__main__':
+    main()
