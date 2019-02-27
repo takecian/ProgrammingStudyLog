3146077d7e38a705f0ab52117fd90cbc5c5dc2bd
takecian <takecian@gmail.com>
2019-02-27 19:38:18 +0900

    wip

---
+++ AtCoder/AGC/027/a.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/agc027/tasks/agc027_a
+
+
+def main():
+    N, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    a.sort()
+
+
+if __name__ == '__main__':
+    main()
