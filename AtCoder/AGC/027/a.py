0861289fbae92b80d3e59bd444539399b850c7ed
takecian <takecian@gmail.com>
2019-02-27 23:04:43 +0900

    Solve

--- AtCoder/AGC/027/a.py
+++ AtCoder/AGC/027/a.py
@@ -6,6 +6,21 @@ def main():
     a = list(map(int, input().split()))
     a.sort()
 
+    count = 0
+    i = 0
+    while x > 0 and i < N:
+        if a[i] <= x:
+            count += 1
+            x -= a[i]
+        else:
+            break
+        i += 1
+
+    if i == N and x != 0:
+        count = max(0, count - 1)
+
+    print(count)
+
 
 if __name__ == '__main__':
     main()
