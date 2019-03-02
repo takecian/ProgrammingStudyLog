f28291a2faadba1c027c17a0228be2c4dccf96c4
takecian <takecian@gmail.com>
2019-03-02 23:30:02 +0900

    Add comment

--- AtCoder/ABC/101/c.py
+++ AtCoder/ABC/101/c.py
@@ -6,6 +6,8 @@ def main():
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
     # print("{}, {}, {}".format(N-1, K-1, (N-1)/(K-1)))
+    # 一度の操作でたかだか K-1 個しか 1 を増やせない
+    # 1 以外の整数の数は N-1 個
     print(math.ceil((N-1)/(K-1)))
 
 
