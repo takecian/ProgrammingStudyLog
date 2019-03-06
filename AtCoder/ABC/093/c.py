95233bb98df3594c0293969d01a0a1492fb80c94
takecian <takecian@gmail.com>
2019-03-06 20:22:29 +0900

    Solve problem

---
+++ AtCoder/ABC/093/c.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc093/tasks/arc094_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    A, B, C = map(int, input().split())
+    count = 0
+
+
+if __name__ == '__main__':
+    main()
