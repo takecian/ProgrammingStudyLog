3cbf47b42610d2b8c4ea0a330a520571c1683a64
takecian <takecian@gmail.com>
2019-03-08 00:20:29 +0900

    Solve

---
+++ AtCoder/ABC/090/a.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abc090/tasks/abc090_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    one = input()
+    two = input()
+    thr = input()
+    print(one[0] + two[1] + thr[2])
+
+if __name__ == '__main__':
+    main()
