6a88d0aa4c9a9f8779ae90b7904e5de6fabbac07
takecian <takecian@gmail.com>
2019-03-08 19:05:13 +0900

    wip

---
+++ AtCoder/ABC/090/d.py
@@ -0,0 +1,21 @@
+# https://atcoder.jp/contests/abc090/tasks/arc091_b
+
+import itertools
+import collections
+import bisect
+
+
+def main():
+    N, K = map(int, input().split())
+    ans = 0
+    for b in range(K+1, N+1):
+        p = N // b
+        r = N - b * p
+        tmp = p * (b - K) + max(0, r - K + 1)
+        # print("b = {}, c = {}, p = {}, tmp = {}".format(b, b - K, p, tmp))
+        ans += tmp
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
