a2db5158066d0032486cc2490f5b046bb94903e8
takecian <takecian@gmail.com>
2019-03-13 22:19:05 +0900

    Solve

---
+++ AtCoder/ABC/084/a.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc084/tasks/abc084_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    M = int(input())
+    print(24 + 24 - M)
+
+if __name__ == '__main__':
+    main()
