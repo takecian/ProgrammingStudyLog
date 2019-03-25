24e80b7c453d573da3aabf0a95b393e0f9d11278
takecian <takecian@gmail.com>
2019-03-25 23:17:23 +0900

    Solve

--- AtCoder/ABC/122/c.py
+++ AtCoder/ABC/122/c.py
@@ -11,16 +11,16 @@ def main():
     counter = [0] * N
     for i in range(N-1):
         if "{}{}".format(S[i], S[i+1]) == 'AC':
-            counter[i+1] = counter[i] + 1
+            counter[i] = 1 + (counter[i - 1] if i > 0 else 0)
         else:
-            counter[i + 1] = counter[i]
+            counter[i] = counter[i - 1] if i > 0 else 0
     # print(counter)
 
     for _ in range(Q):
         l, r = map(int, input().split())
         l -= 1
         r -= 1
-        print(counter[r] - counter[l])
+        print(counter[r - 1] - counter[l - 1])
 
 if __name__ == '__main__':
     main()
