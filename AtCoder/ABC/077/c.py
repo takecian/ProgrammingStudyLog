f11c884c1e138bf3b29d9a7a90d5ccadbb27beca
takecian <takecian@gmail.com>
2019-03-29 15:00:19 +0900

    Solve

--- AtCoder/ABC/077/c.py
+++ AtCoder/ABC/077/c.py
@@ -12,7 +12,16 @@ def main():
     A.sort()
     B.sort()
     C.sort()
-    for a in A:
+
+    ans = 0
+
+    for b in B:
+        i = bisect.bisect_left(A, b)
+        j = bisect.bisect_right(C, b)
+
+        ans += i * (N - j)
+
+    print(ans)
 
 
 if __name__ == '__main__':
