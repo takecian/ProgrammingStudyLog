4c6b21a24de7b2d08e2e3ef0c96582e7e1ab4036
takecian <takecian@gmail.com>
2019-03-29 14:07:37 +0900

    wip

---
+++ AtCoder/ABC/077/c.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/abc077/tasks/arc084_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = list(map(int, input().split()))
+    A.sort()
+    B.sort()
+    C.sort()
+    for a in A:
+
+
+if __name__ == '__main__':
+    main()
---
+++ AtCoder/ABC/078/c.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc078/tasks/arc085_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    N, M = map(int, input().split())
+
+
+if __name__ == '__main__':
+    main()
