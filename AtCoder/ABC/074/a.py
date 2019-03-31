84230a96a80ff3ae4eb14006849ea698d8719312
Takeshi Fujiki <takecian@gmail.com>
2019-03-31 22:59:38 +0900

    Solve

---
+++ AtCoder/ABC/074/a.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc074/tasks/abc074_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    N = int(input())
+    A = int(input())
+    print(N * N - A)
+
+if __name__ == '__main__':
+    main()
