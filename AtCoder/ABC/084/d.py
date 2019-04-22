32465e9db0f60f7f8c7c2b832de37e472af1c040
Takeshi Fujiki <takecian@gmail.com>
2019-04-22 23:46:52 +0900

    Solve

--- AtCoder/ABC/084/d.py
+++ AtCoder/ABC/084/d.py
@@ -10,14 +10,14 @@ def primes(n):
             continue
         for j in range(i * 2, n + 1, i):
             is_prime[j] = False
-    return [i for i in range(n + 1) if is_prime[i]]
+    return is_prime
 
 
 def main():
     p_l = primes(10**5)
     total = [0 for _ in range(10 ** 5 + 1)]
     for i in range(3, 10 ** 5 + 1):
-        if i in p_l and (i + 1) // 2 in p_l:
+        if p_l[i] and p_l[(i + 1) // 2]:
             total[i] = 1 + total[i - 1]
         else:
             total[i] = total[i - 1]
