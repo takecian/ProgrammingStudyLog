732f6b27e5e30f48fbdb8e2e6861b049ee5373f1
takecian <takecian@gmail.com>
2019-04-23 16:48:08 +0900

    Solve

---
+++ hackerrank/Warm-up_Challenges/data_types.py
@@ -0,0 +1,25 @@
+import itertools
+import collections
+import bisect
+
+def main():
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+    # Declare second integer, double, and String variables.
+    ini = int(input())
+    inf = float(input())
+    ins = input()
+
+    # Read and save an integer, double, and String to your variables.
+    print(i + ini)
+    # Print the sum of both integer variables on a new line.
+    print(d + inf)
+    # Print the sum of the double variables on a new line.
+    print(s + ins)
+    # Concatenate and print the String variables on a new line
+    # The 's' variable above should be printed first.
+
+
+if __name__ == '__main__':
+    main()
