3b4adeea1c7ae1c072ef9742c7e9347d21bf235a
takecian <takecian@gmail.com>
2019-05-09 18:40:41 +0900

    Solve

---
+++ hackerrank/data-structures/sort/bubble_sort.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countSwaps function below.
+def countSwaps(a):
+    count = 0
+    for i in range(len(a)):
+        for j in range(len(a) - i - 1):
+            if a[j] > a[j+1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                count += 1
+    print("Array is sorted in {} swaps.".format(count))
+    print("First Element: {}".format(a[0]))
+    print("Last Element: {}".format(a[-1]))
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)
---
+++ hackerrank/data-structures/sort/comparator.py
@@ -0,0 +1,35 @@
+from functools import cmp_to_key
+
+
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def __repr__(self):
+        return "<Name:{}, Age:{}>".format(self.name, self.age)
+
+    def comparator(a, b):
+        if a.score != b.score:
+            return b.score - a.score
+        else:
+            if a.name == b.name:
+                return 0
+            else:
+                if a.name < b.name:
+                    return -1
+                else:
+                    return 1
+
+
+n = int(input())
+data = []
+for i in range(n):
+    name, score = input().split()
+    score = int(score)
+    player = Player(name, score)
+    data.append(player)
+
+data = sorted(data, key=cmp_to_key(Player.comparator))
+for i in data:
+    print(i.name, i.score)
