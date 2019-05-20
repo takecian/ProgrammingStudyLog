d78fc65e4e95065e55ce77b51f8b5dd1267755af
takecian <takecian@gmail.com>
2019-05-20 19:46:02 +0900

    wip

--- AtCoder/ABC/114/d.py
+++ AtCoder/ABC/114/d.py
@@ -1,66 +1,32 @@
 # https://atcoder.jp/contests/abc114/tasks/abc114_d
 
-N = int(input())
+import itertools
+from collections import Counter
+from collections import defaultdict
+import bisect
+
+def main():
+    N = int(input())
+    primes = [0] * (N + 1)
+
+    for i in range(N + 1):
+        cur = i
+        for j in range(2, i + 1):
+            while cur % j == 0:
+                primes[j] += 1
+                cur //= j
+
+    ans = 0
+    for i in range(2, N - 1):
+        for j in range(i + 1, N):
+            for k in range(j + 1, N + 1):
+                # 75
+                if (i == 75 and j == 0 and k == 0) or (i == 0 and j == 75 and k == 0) or (i == 0 and j == 0 and k == 75):
+                    ans += 1
+                else:
+                    if (i == 15 and j == 5 and k == 0) or (i == 0 and j == 15 and k == 0) or (i == 0 and j == 0 and k == 75):
+                # 15, 5
 
 
-def get_primes_dic(v):
-    m = {}
-    i = 2
-
-    while i <= v:
-        while v % i == 0:
-            if i in m:
-                m[i] += 1
-            else:
-                m[i] = 1
-            v //= i
-
-        i += 1
-
-    return m
-
-
-prime_dict = {}
-
-for i in range(1, N+1):
-    d = get_primes_dic(i)
-
-    for k, v in d.items():
-        if k in prime_dict:
-            prime_dict[k] += v
-        else:
-            prime_dict[k] = v
-
-    # print(d)
-
-print(prime_dict)
-
-
-def is_prime(n):
-    if n < 2: return False
-    if n == 2: return True
-    if n % 2 == 0: return False
-
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 1
-
-    return True
-
-
-prime_list = []
-
-for i in range(99):
-    if is_prime(i):
-        prime_list.append(i)
-
-
-for i in range(len(prime_list)-2):
-    for j in range(i+1, len(prime_list)-1):
-        for k in range(j+1, len(prime_list)):
-            i_c = prime_dict[i] if i in prime_dict else 0
-            j_c = prime_dict[j] if j in prime_dict else 0
-            k_c = prime_dict[k] if k in prime_dict else 0
-            if
+if __name__ == '__main__':
+    main()
