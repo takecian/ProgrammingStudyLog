e3820f0bf9c4e344ed00809712827b71fd8cdab1
Takeshi Fujiki <takecian@gmail.com>
2019-05-20 23:34:20 +0900

    ABC126 F

--- AtCoder/ABC/126/f.py
+++ AtCoder/ABC/126/f.py
@@ -6,8 +6,27 @@ import bisect
 
 def main():
     M, K = map(int, input().split())
-    a_len = 2 ** (M + 1)
 
+    if M == 0:
+        if K == 0:
+            print('0 0')
+        else:
+            print('-1')
+        exit()
+    if M == 1:
+        if K == 0:
+            print('0 0 1 1')
+        else:
+            print('-1')
+        exit()
+
+    if 2 ** M <= K:
+        print('-1')
+    else:
+        a = [i for i in range(2 ** M) if i != K]
+        b = reversed(a)
+        ans = ' '.join(map(str, a)) + ' ' + str(K) + ' ' + ' '.join(map(str, b)) + ' ' + str(K)
+        print(ans)
 
 if __name__ == '__main__':
     main()
