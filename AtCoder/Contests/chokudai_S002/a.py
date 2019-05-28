458274e29adbadbedcb9f47a017cddf84fb98832
takecian <takecian@gmail.com>
2019-05-28 16:25:44 +0900

    Speedrun 2

---
+++ AtCoder/Contests/chokudai_S002/a.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/chokudai_S002/tasks/chokudai_S002_a
+
+import itertools
+import collections
+import bisect
+
+def main():
+    N = int(input())
+    for _ in range(N):
+        a, b = map(int, input().split())
+        print(a*b)
+
+if __name__ == '__main__':
+    main()
