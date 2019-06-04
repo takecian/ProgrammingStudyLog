b2769c2a43809dc82174372b17e7606be144b8ff
Takeshi Fujiki <takecian@gmail.com>
2019-06-04 23:46:43 +0900

    Leetcode 56

--- LeetCode/51-60/56.py
+++ LeetCode/51-60/56.py
@@ -8,29 +8,35 @@ from heapq import heappush, heappop
 
 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def merge(self, intervals):
+        intervals.sort(key=lambda x: x[0])
         ans = []
         i = 0
         while i < len(intervals):
             b = intervals[i]
+            # print(b)
             start = b[0]
             end = b[1]
             j = i + 1
             while j < len(intervals):
                 ne = intervals[j]
-                if end <= ne[0]:
+                # print(ne)
+                if ne[0] <= end:
                     end = max(end, ne[1])
-                    i = j
                     j += 1
+                    i = j
                 else:
+                    i = j
                     break
             ans.append([start, end])
+            if j == len(intervals):
+                break
 
         return ans
 
 def main():
     s = Solution()
-    print(s.merge([[1,4],[4,5]]))
+    print(s.merge([[1,3],[2,6],[8,10],[15,18]]))
 
 if __name__ == '__main__':
     main()
