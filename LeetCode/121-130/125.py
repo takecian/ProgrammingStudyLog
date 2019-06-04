247d4d4a970af58a6089d33a67cb97cc2d426e88
Takeshi Fujiki <takecian@gmail.com>
2019-06-04 09:35:36 +0900

    Leetcode 125

---
+++ LeetCode/121-130/125.py
@@ -0,0 +1,24 @@
+import itertools
+from collections import Counter
+from collections import defaultdict
+import bisect
+from heapq import heappush, heappop
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = list(filter(lambda c: c.isalpha(), s))
+        alpha_string = ''.join(l).lower()
+        rev_alpha_string = alpha_string.reverse()
+        print(alpha_string)
+        print(rev_alpha_string)
+        return alpha_string == rev_alpha_string
+
+def main():
+    s = Solution()
+    print(s.isPalindrome('A man, a plan, a canal: Panama'))
+    print(s.isPalindrome('OP'))
+
+
+if __name__ == '__main__':
+    main()
