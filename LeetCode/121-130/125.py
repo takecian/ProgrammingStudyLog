aeed7502a7259ac56f2c04b09657b129f8bb2ce2
Takeshi Fujiki <takecian@gmail.com>
2019-06-04 23:36:10 +0900

    Leetcode 125

--- LeetCode/121-130/125.py
+++ LeetCode/121-130/125.py
@@ -7,11 +7,11 @@ from heapq import heappush, heappop
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l = list(filter(lambda c: c.isalpha(), s))
+        l = list(filter(lambda c: c.isalnum(), s))
         alpha_string = ''.join(l).lower()
-        rev_alpha_string = alpha_string.reverse()
-        print(alpha_string)
-        print(rev_alpha_string)
+        rev_alpha_string = alpha_string[::-1]
+        # print(alpha_string)
+        # print(rev_alpha_string)
         return alpha_string == rev_alpha_string
 
 def main():
