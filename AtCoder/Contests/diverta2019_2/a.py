16c949493ecb9c043d7ef68d5c7001d460234281
Takeshi Fujiki <takecian@gmail.com>
2019-06-15 20:42:23 +0900

    Prepare diverta2019

---
+++ AtCoder/Contests/diverta2019_2/a.py
@@ -0,0 +1,13 @@
+#
+
+import itertools
+from collections import Counter
+from collections import defaultdict
+import bisect
+from heapq import heappush, heappop
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
