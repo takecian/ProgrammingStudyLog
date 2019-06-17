67aec53bf675fb14ab3ac7f9a85d54228d12a3c6
Takeshi Fujiki <takecian@gmail.com>
2019-06-17 12:36:45 +0900

    Fix LIS

--- Libs/lis.py
+++ Libs/lis.py
@@ -5,8 +5,8 @@ def LIS(L):
     seq = []
     for i in L:
         pos = bisect(seq, i)
-    if len(seq) <= pos:  # 既存の部分列のすべてより大きいなら追加する
-        seq.append(i)
-    else:  # 既存の部分列の中に含まれるんだったら、自分より大きくて一番小さいものと差し替える
-        seq[pos] = i
+        if len(seq) <= pos:  # 既存の部分列のすべてより大きいなら追加する
+            seq.append(i)
+        else:  # 既存の部分列の中に含まれるんだったら、自分より大きくて一番小さいものと差し替える
+            seq[pos] = i
     return len(seq)
