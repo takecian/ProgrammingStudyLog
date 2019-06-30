77d75e1d34def4d7612a7ae087f660af71b6c673
Takeshi Fujiki <takecian@gmail.com>
2019-06-30 22:17:37 +0900

    Leetcode 206

---
+++ LeetCode/200/206.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        current = head
+        prev = None
+        while current:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        return prev
