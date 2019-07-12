22a53e451b67a357f0e9f1c61582ac6a67cec97a
takecian <takecian@gmail.com>
2019-07-12 17:52:22 +0900

    Leetcode 146

--- LeetCode/100/146.py
+++ LeetCode/100/146.py
@@ -37,3 +37,34 @@ class LRUCache:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+from collections import OrderedDict
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.dic = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+
+        val = self.dic.pop(key)
+        self.dic[key] = val
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.dic.pop(key)
+        else:
+            if self.capacity == len(self.dic):
+                self.dic.popitem(False)
+
+        self.dic[key] = value
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
--- README.md
+++ README.md
@@ -163,6 +163,9 @@
 * 重複を取り除くのに使う
   * https://leetcode.com/problems/letter-tile-possibilities/
 
+* 順序を保持する dictionary もある、OrderedDict
+  * https://leetcode.com/problems/lru-cache/
+
 #### 貪欲法
 何か優先順位をつけて、その順番で処理していく
 * 大きい方(小さい方)から入る分だけ詰めてみる
