124443113524802be7a965d23e5a6565c120c832
takecian <takecian@gmail.com>
2019-07-19 14:08:02 +0900

    Leetcode 139

---
+++ LeetCode/100/139.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(len(dp)):
+            if dp[i]:
+                sub_str = s[i:]
+                for w in wordDict:
+                    if sub_str.find(w) == 0:
+                        dp[i + len(w)] = True
+
+        return dp[-1]
