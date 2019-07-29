49432e51390a44b4626b41c9a4eee8f55cbf86e9
Takeshi Fujiki <takecian@gmail.com>
2019-07-29 13:29:50 +0900

    Mock interview goole

---
+++ LeetCode/1000/1095.py
@@ -0,0 +1,60 @@
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+# class MountainArray(object):
+#    def get(self, index):
+#        """
+#        :type index: int
+#        :rtype int
+#        """
+#
+#    def length(self):
+#        """
+#        :rtype int
+#        """
+
+class Solution(object):
+    def findInMountainArray(self, target, mountain_arr):
+        """
+        :type target: integer
+        :type mountain_arr: MountainArray
+        :rtype: integer
+        """
+
+        # find center
+        l = 0
+        r = mountain_arr.length() - 1
+        while l < r:
+            m = (l + r) / 2
+            if mountain_arr.get(m) < mountain_arr.get(m + 1):
+                l = peak = m + 1
+            else:
+                r = m
+        # print(m)
+        # find right side
+        l = 0
+        r = peak
+        while l <= r:
+            c = (l + r) // 2
+            if mountain_arr.get(c) == target:
+                return c
+
+            if mountain_arr.get(c) < target:
+                l = c + 1
+            else:
+                r = c - 1
+        # find left side
+        l = peak + 1
+        r = mountain_arr.length() - 1
+        while l <= r:
+            c = (l + r) // 2
+            if mountain_arr.get(c) == target:
+                return c
+
+            if mountain_arr.get(c) > target:
+                l = c + 1
+            else:
+                r = c - 1
+
+        return -1
