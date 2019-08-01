2d59a8d52933c5f336f2212cd4db4f4790fb9e65
Takeshi Fujiki <takecian@gmail.com>
2019-08-02 00:47:01 +0900

    Leetcode 43

--- LeetCode/0/43.py
+++ LeetCode/0/43.py
@@ -1,29 +1,19 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        len1 = len(num1)
-        len2 = len(num2)
-        result = [0] * (len1 + len2)
+        n = len(num1)
+        m = len(num2)
+        length = n + m
+        result = [0] * length
 
-        for i in range(len1 - 1, -1, -1):
-            for j in range(len2 - 1, -1, -1):
-                p1 = i + j
-                p2 = i + j + 1
-                prod = int(num1[i]) * int(num2[j]) + result[p2]
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                pos1 = i + j
+                pos2 = i + j + 1
+                val = int(num1[i]) * int(num2[j]) + result[pos2]
+                result[pos1] += val // 10
+                result[pos2] = val % 10
 
-                result[p1] += prod // 10
-                result[p2] = prod % 10
-                print(result)
-
-        ans = ''
-        leading_zero = True
-        for c in result:
-            if c != 0:
-                leading_zero = False
-            if not leading_zero:
-                ans += str(c)
-
-        return ans if len(ans) > 0 else '0'
-
-
-s = Solution()
-print(s.multiply('123', '123'))
+        offset = 0
+        while offset < length - 1 and result[offset] == 0:
+            offset += 1
+        return ''.join(map(str, result[offset:]))
