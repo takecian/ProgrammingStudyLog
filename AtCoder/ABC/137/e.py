f53948c6588e4ebe300a2d0b3b589b1ce965ff2f
takecian <takecian@gmail.com>
2019-08-20 12:26:18 +0900

    ABC 137 E

--- AtCoder/ABC/137/e.py
+++ AtCoder/ABC/137/e.py
@@ -1,6 +1,7 @@
+# https://atcoder.jp/contests/abc137/tasks/abc137_e
 
-#
-
+import sys
+sys.setrecursionlimit(12345678)
 import itertools
 from collections import Counter
 from collections import defaultdict
@@ -10,26 +11,73 @@ from heapq import heappush, heappop
 
 def main():
     n, m, p = map(int, input().split())
+    edges = []
     graph = defaultdict(list)
-    coins = {}
+    rev_graph = defaultdict(list)
+
     for _ in range(m):
         a, b, c = map(int, input().split())
-        graph[a-1].append(b-1)
-        coins[(a-1,b-1)] = -(c - p)
+        a -= 1
+        b -= 1
+        c = -(c - p)
+        edges.append((a, b, c))
+        graph[a].append(b)
+        rev_graph[b].append(a)
+
+    # pick up nodes on goal path
+    reachable_from_start = [False] * n
+
+    # print(edges)
+    def dfs(node):
+        if reachable_from_start[node]:
+            return
+        reachable_from_start[node] = True
+        for next_node in graph[node]:
+            dfs(next_node)
+
+    reachable_to_end = [False] * n
+
+    def rdfs(node):
+        if reachable_to_end[node]:
+            return
+        reachable_to_end[node] = True
+        for next_node in rev_graph[node]:
+            rdfs(next_node)
+
+    dfs(0)
+    rdfs(n-1)
+
+    valid = [False] * n
+    for i in range(n):
+        valid[i] = reachable_from_start[i] and reachable_to_end[i]
 
     inf = 10**10
     scores = [inf] * n
     scores[0] = 0
+    loop = 0
+
     # bellman-ford
-    for loop in range(2):
-        for i in range(n):
-            for next_node in graph[i]:
-                scores[next_node] = min(scores[next_node], scores[i] + coins[(i, next_node)])
+    updated = True
+    while updated:
+        updated = False
+        for i in range(m):
+            a, b, c = edges[i]
+            if not valid[a]:
+                continue
+            if not valid[b]:
+                continue
 
-        if loop == 0:
-            ans = scores[-1]
+            new_cost = scores[a] + c
+            if new_cost < scores[b]:
+                updated = True
+                scores[b] = new_cost
 
-    print(-1 if ans != scores[-1] or ans == inf else -ans)  # n 回移動した時点よりスコアが良くなってたら無限に上げられる
+        loop += 1
+        if loop > n:
+            print(-1)
+            return
+
+    print(max(0, -scores[-1]))
 
 
 if __name__ == '__main__':
