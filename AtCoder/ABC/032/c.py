4d59299e691feebc5da2c64de0c8866a5f097e45
takecian <takecian@gmail.com>
2019-08-26 18:34:44 +0900

    ABC C

--- AtCoder/ABC/032/c.py
+++ AtCoder/ABC/032/c.py
@@ -9,6 +9,31 @@ from heapq import heappush, heappop
 
 
 def main():
+    n, k = map(int, input().split())
+    sl = [int(input()) for _ in range(n)]
+
+    if min(sl) == 0:
+        print(n)
+        return
+
+    if k < min(sl):
+        print(0)
+        return
+
+    l = 0
+    val = 1
+    ans = 0
+    for r in range(n):
+        val *= sl[r]
+        if k < val:
+            while k < val and l < r:
+                val //= sl[l]
+                l += 1
+
+        if val <= k:
+            ans = max(ans, r - l + 1)
+
+    print(ans)
 
 
 if __name__ == '__main__':
