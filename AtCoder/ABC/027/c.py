6adbfb0d9024a3607eaef030292728351e2642c3
takecian <takecian@gmail.com>
2019-08-27 13:11:58 +0900

    ABC C

--- AtCoder/ABC/027/c.py
+++ AtCoder/ABC/027/c.py
@@ -10,26 +10,37 @@ import bisect
 from heapq import heappush, heappop
 
 
-def f(x):
-    y = 1
-    e = 1
-    if x == 1:
-        return False
+# コピペしてきた、ちゃんと理解してないのでまた調べる
+def f(N):
+    win = 1
+
+    left = N + 1
+    result = win
 
     while True:
-        e *= 4
-        y += e
-        if x <= y:
-            return True
-        y += e
-        if x <= y:
-            return False
+        if left == 1:
+            break
+        x = left // 2
+        right = left - 1
+        if left % 2 == 0:
+            left = x
+        else:
+            # xだと選べる
+            if result == win:
+                left = x + 1
+            else:
+                left = x
+        result = 1 - result
+        continue
+
+    answer = 'Takahashi' if result == win else 'Aoki'
+    print(answer)
 
 
 def main():
     n = int(input())
 
-    print('Takahashi' if f(n) else 'Aoki')
+    f(n)
 
 
 if __name__ == '__main__':
