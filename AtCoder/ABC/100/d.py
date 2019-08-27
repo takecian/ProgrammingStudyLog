aa013155aca53b110c6b3d7d87c3f88b0e0e13bc
takecian <takecian@gmail.com>
2019-08-27 16:40:06 +0900

    ABC D

--- AtCoder/ABC/100/d.py
+++ AtCoder/ABC/100/d.py
@@ -62,10 +62,30 @@ def calc8(tup):
 def main():
     n, m = map(int, input().split())
     xyzl = []
-    for _ in range(len(n)):
+    for _ in range(n):
         x, y, z = map(int, input().split())
         xyzl.append((x,y,z))
 
+    ans = 0
+    xyzl.sort(key=lambda xyz: -calc1(xyz))
+    ans = max(ans, sum([calc1(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc2(xyz))
+    ans = max(ans, sum([calc2(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc3(xyz))
+    ans = max(ans, sum([calc3(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc4(xyz))
+    ans = max(ans, sum([calc4(xyz) for xyz in xyzl[:m]]))
+
+    xyzl.sort(key=lambda xyz: -calc5(xyz))
+    ans = max(ans, sum([calc5(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc6(xyz))
+    ans = max(ans, sum([calc6(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc7(xyz))
+    ans = max(ans, sum([calc7(xyz) for xyz in xyzl[:m]]))
+    xyzl.sort(key=lambda xyz: -calc8(xyz))
+    ans = max(ans, sum([calc8(xyz) for xyz in xyzl[:m]]))
+
+    print(ans)
 
 
 if __name__ == '__main__':
