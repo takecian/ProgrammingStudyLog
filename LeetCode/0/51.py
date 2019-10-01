11a94d3838e6ff24e684e0f4c0c76a87ece94c7f
takecian <takecian@gmail.com>
2019-10-01 10:50:25 +0900

    51

--- LeetCode/0/51.py
+++ LeetCode/0/51.py
@@ -4,113 +4,47 @@ class Solution:
         ans = []
 
         def can_put(h, w):
-            # right top
-            ch = h
-            cw = w
-            while 0 <= ch and cw < n:
-                if board[ch][cw] == 'Q':
-                    return False
-                ch -= 1
-                cw += 1
-            # left top
-            ch = h
-            cw = w
-            while 0 <= ch and 0 <= cw:
-                if board[ch][cw] == 'Q':
-                    return False
-                ch -= 1
-                cw -= 1
-            # right down
-            ch = h
-            cw = w
-            while ch < n and cw < n:
-                if board[ch][cw] == 'Q':
-                    return False
-                ch += 1
-                cw += 1
-            # left down
-            ch = h
-            cw = w
-            while ch < n and 0 <= cw:
-                if board[ch][cw] == 'Q':
-                    return False
-                ch += 1
-                cw -= 1
+            if queen_row[h] == 1:
+                return False
+            if h + w in upright:
+                return False
+            if h - w in downright:
+                return False
             return True
 
         def put(h, w):
-            # right top
-            ch = h
-            cw = w
-            while 0 <= ch and cw < n:
-                board[ch][cw] = 'Q'
-                ch -= 1
-                cw += 1
-            # left top
-            ch = h
-            cw = w
-            while 0 <= ch and 0 <= cw:
-                board[ch][cw] = 'Q'
-                ch -= 1
-                cw -= 1
-            # right down
-            ch = h
-            cw = w
-            while ch < n and cw < n:
-                board[ch][cw] = 'Q'
-                ch += 1
-                cw += 1
-            # left down
-            ch = h
-            cw = w
-            while ch < n and 0 <= cw:
-                board[ch][cw] = 'Q'
-                ch += 1
-                cw -= 1
+            queens.add((h, w))
+            queen_row[h] = 1
+            upright.add(h + w)
+            downright.add(h - w)
 
         def remove(h, w):
-            # right top
-            ch = h
-            cw = w
-            while 0 <= ch and cw < n:
-                board[ch][cw] = ''
-                ch -= 1
-                cw += 1
-            # left top
-            ch = h
-            cw = w
-            while 0 <= ch and 0 <= cw:
-                board[ch][cw] = ''
-                ch -= 1
-                cw -= 1
-            # right down
-            ch = h
-            cw = w
-            while ch < n and cw < n:
-                board[ch][cw] = ''
-                ch += 1
-                cw += 1
-            # left down
-            ch = h
-            cw = w
-            while ch < n and 0 <= cw:
-                board[ch][cw] = ''
-                ch += 1
-                cw -= 1
-            return True
+            queens.remove((h, w))
+            queen_row[h] = 0
+            upright.remove(h + w)
+            downright.remove(h - w)
 
         def backtrack(w):
             nonlocal ans
-            print(w, board)
-            if w == n - 1:
-                yeah = [[board[ph][pw] for pw in range(n)] for ph in range(n)]
-                ans.append(yeah)
+            if len(queens) == n:
+                solution = [['' for _ in range(n)] for _ in range(n)]
+                for h in range(n):
+                    for w in range(n):
+                        if (h, w) in queens:
+                            solution[h][w] = 'Q'
+                solution = [''.join(solution[h]) for h in range(n)]
+                ans.append(solution)
                 return
+
             for h in range(n):
                 if can_put(h, w):
                     put(h, w)
                     backtrack(w + 1)
                     remove(h, w)
 
+        queens = set()
+        queen_row = [0] * n
+        upright = set()
+        downright = set()
         backtrack(0)
         return ans
