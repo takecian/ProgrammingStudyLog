7df9692979ba14f08083be444f043ca22e8cffee
takecian <takecian@gmail.com>
2019-10-25 18:58:22 +0900

    Leetcode

--- LeetCode/300/392.py
+++ LeetCode/300/392.py
@@ -11,3 +11,29 @@ class Solution:
                 ti += 1
 
         return si == len(s)
+
+
+from collections import defaultdict
+import bisect
+
+    def isSubsequence2(self, s: str, t: str) -> bool:
+
+        charindex = defaultdict(list)
+        for i in range(len(t)):
+            charindex[t[i]].append(i)
+
+        si = 0
+        ti = 0
+        while si < len(s):
+            ilist = charindex[s[si]]
+            if len(ilist) == 0:
+                return False
+
+            index = bisect.bisect_left(ilist, ti)
+            if index == len(ilist):
+                return False
+
+            si += 1
+            ti = ilist[index] + 1
+
+        return True
