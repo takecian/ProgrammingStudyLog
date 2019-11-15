3947fdb144d99b2036a305e01b0473161c813f88
takecian <takecian@gmail.com>
2019-11-15 19:17:11 +0900

    WIP PAST

--- AtCoder/past-sample/e.py
+++ AtCoder/past-sample/e.py
@@ -10,7 +10,21 @@ from heapq import heappush, heappop
 
 def main():
     n, m = map(int, input().split())
+    cl = [[int(i) for i in input().split()] + [j] for j in range(m)]
 
+    # print(c_l)
+    cl.sort(key=lambda x: x[1])
+
+    counter = [0] * (n + 1)
+    for c in cl:
+        p = c[0]
+        counter[p] += 1
+        c.append("{:06}{:06}".format(c[0], counter[p]))
+
+    cl.sort(key=lambda x: x[2])
+
+    for c in cl:
+        print(c[3])
 
 
 if __name__ == '__main__':
