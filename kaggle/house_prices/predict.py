93ad764789ab4fd3fd34bd6738d73182e5f181e8
takecian <takecian@gmail.com>
2019-12-16 19:49:27 +0900

    Update

--- kaggle/house_prices/predict.py
+++ kaggle/house_prices/predict.py
@@ -16,23 +16,27 @@ def main():
     print(train.dtypes)
 
     # msno.matrix(df=train, figsize=(20, 14), color=(0.5, 0, 0))
-    sns.heatmap(train.isnull(), cbar=False)
-    return
+    # sns.heatmap(train.isnull(), cbar=False)
+
     # 学習データを特徴量と目的変数に分ける
     train_x = train.drop([target_key], axis=1)
     train_y = train[target_key]
+    train_y = np.log(train_y)
 
     # テストデータは特徴量のみなので、そのままでよい
     test_x = test.copy()
 
-    necessary_number_keys = ['LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2',
-                             'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'GarageCars', 'GarageArea']
-    necessary_num_zero_keys = ['GarageYrBlt', 'WoodDeckSF']
+    necessary_number_keys = ['OverallQual', 'TotalSF', 'YearBuilt', 'GarageArea']
+    necessary_num_zero_keys = []
 
     category_keys = ['MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood',
                      'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle','Foundation','BsmtQual','BsmtCond',
                      'BsmtExposure','BsmtFinType1','Heating','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',
                      'GarageType', 'GarageFinish','GarageQual','Fence','SaleType','SaleCondition']
+
+    train_x['TotalSF'] = train_x['TotalBsmtSF'] + train_x['1stFlrSF'] + train_x['2ndFlrSF']
+    test_x['TotalSF'] = test_x['TotalBsmtSF'] + test_x['1stFlrSF'] + test_x['2ndFlrSF']
+
     # 不要カラムの削除
     train_x = train_x[necessary_number_keys + necessary_num_zero_keys + category_keys]
     test_x = test_x[necessary_number_keys + necessary_num_zero_keys + category_keys]
@@ -61,17 +65,19 @@ def main():
 
     # モデルの作成および学習データを与えての学習
     from sklearn import linear_model
-    model = linear_model.LinearRegression()
+    from sklearn.ensemble import RandomForestRegressor
+    model = RandomForestRegressor(n_estimators=80, max_features='auto')
     model.fit(train_x, train_y)
 
     # テストデータの予測値を出力する
     pred = model.predict(test_x)
     # print('pred')
     # print(pred)
+    y_pred = np.exp(pred)
 
     # 提出用ファイルの作成
-    submission = pd.DataFrame({'Id': test['Id'], 'SalePrice': pred})
-    submission.to_csv('submission_first.csv', index=False)
+    submission = pd.DataFrame({'Id': test['Id'], 'SalePrice': y_pred})
+    submission.to_csv('submission_random.csv', index=False)
     print('Done.')
 
 if __name__ == '__main__':
