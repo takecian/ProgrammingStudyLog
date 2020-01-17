1e596e7706101c34fe36a25c10f3d2683caa49ea
takecian <takecian@gmail.com>
2020-01-17 12:38:09 +0900

    Update

--- LeetCode/0/76.py
+++ LeetCode/0/76.py
@@ -5,23 +5,24 @@ import bisect
 from heapq import heappush, heappop
 
 class Solution:
-    def minWindow(self, s, t):
+    def minWindow(self, S, T):
         ans = ''
 
-        n = len(s)
-        t_counter = Counter(t)
+        n = len(S)
+        t_counter = Counter(T)
         counter = defaultdict(lambda: 0)
         covered = 0
         start = 0
         min_len = 10**10
 
-        for i, c in enumerate(s):
+        for i, c in enumerate(S):
+            # add last char int counter
             counter[c] += 1
             if c in t_counter and counter[c] == t_counter[c]:
                 covered += 1
             while start < n:
-                char = s[start]
-                if char not in t or counter[char] > t_counter[char]:
+                char = S[start]
+                if char not in T or counter[char] > t_counter[char]:
                     # we can proceed
                     counter[char] -= 1
                     start += 1
@@ -31,7 +32,7 @@ class Solution:
             length = i - start + 1
             if covered == len(t_counter) and length < min_len:
                 min_len = length
-                ans = s[start:(i + 1)]
+                ans = S[start:(i + 1)]
         return ans
 
 def main():
