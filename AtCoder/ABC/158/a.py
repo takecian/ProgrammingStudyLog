f78e2fdfaf298f7e10e1136ed31b9ad7ff969a52
Takeshi Fujiki <takecian@gmail.com>
2020-03-20 23:38:43 +0900

    ABC

---
+++ AtCoder/ABC/158/a.py
@@ -0,0 +1,21 @@
+# https://atcoder.jp/contests/abc158/tasks/abc158_a
+import sys
+sys.setrecursionlimit(12345678)
+import itertools
+from collections import Counter
+from collections import defaultdict
+from collections import deque
+import bisect
+from heapq import heappush, heappop
+
+
+def main():
+    s = input()
+    if s == 'AAA' or s == 'BBB':
+        print('No')
+    else:
+        print('Yes')
+    return
+
+if __name__ == '__main__':
+    main()
