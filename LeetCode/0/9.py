bbfba7f65625ced52220df30c28185368e3e775b
Takeshi Fujiki <takecian@gmail.com>
2021-03-12 23:23:43 +0900

    Update

--- LeetCode/0/9.py
+++ LeetCode/0/9.py
@@ -8,4 +8,22 @@ def isPalindrome(x):
     """
     return str(x) == str(x)[::-1]
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or x != 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+        while x > reversed_num:
+            reversed_num = reversed_num * 10 + temp % 10
+            temp = temp // 10
+
+        print(x)
+        print(reversed_num)
+        return x == reversed_num
+
+s = Solution()
+print(s.isPalindrome(121))
+
 isPalindrome(121)
