f076baecf65587938c2653de2ea8a51c2077ba77
Takeshi Fujiki <takecian@gmail.com>
2021-03-13 22:52:44 +0900

    Update

--- LeetCode/300/347.py
+++ LeetCode/300/347.py
@@ -1,19 +1,35 @@
 from collections import defaultdict
 from heapq import heappush, heappop
+from typing import List
 
+def topKFrequent(nums: List[int], k: int) -> List[int]:
+    counter = defaultdict(int)
+    for n in nums:
+        counter[n] += 1
 
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = defaultdict(int)
-        for n in nums:
-            counter[n] += 1
+    que = []
+    for num, count in counter.items():
+        heappush(que, (-count, num))
 
-        que = []
-        for num, count in counter.items():
-            heappush(que, (-count, num))
+    ans = []
+    c = len(que)
+    for _ in range(c):
+        count, num = heappop(que)
+        ans.append(num)
+    print(ans)
+    return ans[k-1]
 
-        ans = []
-        for _ in range(k):
-            count, num = heappop(que)
-            ans.append(num)
-        return ans
+def solve2(nums: List[int], k: int) -> List[int]:
+    counter = defaultdict(int)
+    for n in nums:
+        counter[n] += 1
+    print(counter)
+    print(counter.items())
+    sorted_counter = sorted(counter.items(), key=lambda x:-x[1])
+    print(sorted_counter)
+    return sorted_counter[k-1][0]
+
+print(topKFrequent(["John", "Kevin", "Kevin", "Bob", "Bob", "Bob"], 2))
+
+print(solve2(["John", "Kevin", "Kevin", "Bob", "Bob", "Bob"], 2))
+
